c4e981bfede19c10065617b54c389c781ffc15a3
Sleepypalm <103427352+Sleepypalm@users.noreply.github.com>
2024-01-31 13:38:07 +0100

    létre hoztam egy ul listát list id-val a gomb meghívásánál kiválasztom a list id-val rendelkező Htmlelement-et üressé teszem a belsejét végig iterálok az elemein majd létre hozok li elemeket és feltöltőm a tömb elemeivel vesszővel elválasztva végül hozzácsatolom az ul-hez

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,12 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <label for"input">Item: </label>
     <input type="number" name="input" id="input">
     <button  id="btn" >Add new item</button>
+
+    <ul id="list">
+    
+    </ul>
   </div>
+
 `;
 
 document.getElementById("btn")?.addEventListener("click", () => {
@@ -21,5 +26,13 @@ document.getElementById("btn")?.addEventListener("click", () => {
     numberArray.push(number);
     console.log(numberArray);
     (document.getElementById("input") as HTMLInputElement).value = "";
+
+    let list = document.getElementById("list") as HTMLElement;
+    list.innerHTML = "";
+    numberArray.forEach((element) => {
+      let li = document.createElement("li");
+      li.innerHTML = element.toString() + ",";
+      list?.append(li);
+    });
   }
 });
